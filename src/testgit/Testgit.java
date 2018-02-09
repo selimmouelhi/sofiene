b74bae8097cf1947d9284202ce492ab0f7c4b323
selimmouelhi <slmmlh30@gmail.com>
2018-02-09 12:11:41 +0100

    configuration main

--- src/testgit/Testgit.java
+++ src/testgit/Testgit.java
@@ -15,6 +15,8 @@ public class Testgit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("sofiene");
         // TODO code application logic here
     }
     
